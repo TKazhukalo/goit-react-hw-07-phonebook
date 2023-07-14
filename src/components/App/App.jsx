515dee4cf6f44a90f4eb3b442262606b96874722
TKazhukalo <118135022+TKazhukalo@users.noreply.github.com>
2023-07-14 22:56:26 +0300

    create loader

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,14 +4,17 @@ import { ContactsList } from 'components/ContactsList/ContactsList';
 import { Filter } from 'components/Filter/Filter';
 import { Form } from 'components/Form/Form';
 import { Section } from 'components/Section/Section';
-import { selectContacts, selectStatusFilters } from 'redux/selectors';
+import { selectContacts, selectError, selectIsLoading, selectStatusFilters } from 'redux/selectors';
 import { useEffect } from 'react';
 import { fetchContacts } from 'redux/operations';
+import Loader from 'components/Loader/Loader';
 
 export const App = () => {
   const contacts = useSelector(selectContacts);
   const value = useSelector(selectStatusFilters);
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -28,6 +31,8 @@ export const App = () => {
         ) : (
           <p>Введіть дані</p>
         )}
+        {isLoading && !error && <Loader />}
+{/*{isLoading && !error && <b>Request in progress...</b>}*/}
         <ContactsList />
       </Section>
     </Container>
